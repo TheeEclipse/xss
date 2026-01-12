7808605017f13e75636d96b1be447cc39e9c27b9
Kelvin <wanjakelvinndungu@gmail.com>
2026-01-12 13:31:59 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -3,3 +3,4 @@ alert('TheeEclipse')
 // https://cdn.jsdelivr.net/ will allow you to proxy any file on github.com
 // https://cdn.jsdelivr.net/gh/<username>/<project>/<filename>
 // https://cdn.jsdelivr.net/gh/renniepak/xss/xss.js
+ // so: https://cdn.jsdelivr.net/gh/TheeEclipse/xss/xss.js loads this js to bupass CSP
